4d9f0093254e98b8223330e0ed8089f35161c7ec
Alexander Huang <email": null>
2015-09-15 16:02:35 -0400

    uber api in main.js WIP

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,22 +1,21 @@
 $(document).ready(function() {
-  $.ajax({
-    url: 'https://api.uber.com/v1/products',
-    headers: {Authorization: "Token " + 'cuuAQ79Q2L3SKGm6xHVR85NXkkzvEht8aNL1tNRk'},
-    data: {
-      latitude: 40.7063634,
-      longitude: 74.00909630000001
-    }
-  }).done(function(response) {
-    console.log('it works');
-  }).fail(function(response) {
-    console.log('it failed');
-  })
+
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://api.uber.com/v1/products?latitude=40.7063634&longitude=-74.009096');
+  xhr.setRequestHeader("Authorization", "Token cuuAQ79Q2L3SKGm6xHVR85NXkkzvEht8aNL1tNRk");
+
+  // $.ajax({
+  //   url: 'https://api.uber.com/v1/products',
+  //   // headers: {Authorization: "Token " + 'cuuAQ79Q2L3SKGm6xHVR85NXkkzvEht8aNL1tNRk'},
+  //   data: {
+  //     server_token: 'cuuAQ79Q2L3SKGm6xHVR85NXkkzvEht8aNL1tNRk',
+  //     latitude: 40.7063634,
+  //     longitude: -74.00909630000001
+  //   }
+  // }).done(function(response) {
+  //   console.log('it works');
+  // }).fail(function(response) {
+  //   console.log(arguments);
+  //   console.log('it failed');
+  // })
 })
-
-
-
-    // data: parameters = {
-    // 'server_token': 'cuuAQ79Q2L3SKGm6xHVR85NXkkzvEht8aNL1tNRk',
-    // 'latitude': 37.775818,
-    // 'longitude': -122.418028,
-    // }
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,5 @@ class WelcomeController < ApplicationController
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   def index
-
   end
 end
